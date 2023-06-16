914b669277d186639e12130b0248443ebf239624
hmtheboy154 <buingoc67@gmail.com>
2023-06-16 11:01:32 -0400

    Android.mk : Provide cpp lib of inih
    also header libraries
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- Android.mk
+++ Android.mk
@@ -10,8 +10,24 @@ LOCAL_SRC_FILES :=		\
 	ini.c	
 include $(BUILD_SHARED_LIBRARY) 
 
+##Build libinih-cpp
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+LOCAL_CFLAGS := 
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := libinih-cpp
+LOCAL_SRC_FILES :=	ini.c \
+	cpp/INIReader.cpp
+include $(BUILD_SHARED_LIBRARY) 
+
 ##Make libinih_headers
 include $(CLEAR_VARS)
 LOCAL_MODULE := libinih_headers
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+include $(BUILD_HEADER_LIBRARY)
+
+##Make libinih-cpp_headers
+include $(CLEAR_VARS)
+LOCAL_MODULE := libinih-cpp_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/cpp
 include $(BUILD_HEADER_LIBRARY)
